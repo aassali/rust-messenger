60ed909bb097ec4aa5210db3c51b1374db388990
fabien <fabien.rmnd@gmail.com>
2021-12-05 23:26:12 +0100

    add server and client

--- src/client.rs
+++ src/client.rs
@@ -1,18 +1,54 @@
 use std::net::TcpStream;
-use std::env;
+use std::io::{Write, Read, stdin, stdout};
+
+fn get_entry() -> String {
+    let mut buf = String::new();
+
+    stdin().read_line(&mut buf);
+    buf.replace("\n", "").replace("\r", "")
+}
+
+fn exchange_with_server(mut stream: TcpStream) {
+    let stdout = std::io::stdout();
+    let mut io = stdout.lock();
+    let mut buf = &mut [0; 3];
+
+    println!("Entrez 'quit' pour quitter");
+    loop {
+        write!(io, "> ");
+        // pour afficher de suite
+        io.flush();
+        match &*get_entry() {
+            "quit" => {
+                println!("bye !");
+                return;
+            }
+            line => {
+                write!(stream, "{}\n", line);
+                match stream.read(buf) {
+                    Ok(received) => {
+                        if received < 1 {
+                            println!("Perte de la connexion avec le serveur");
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        println!("Perte de la connexion avec le serveur");
+                        return;
+                    }
+                }
+                println!("Réponse du serveur : {:?}", buf);
+            }
+        }
+    }
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let name = &args[1];
-
-    println!("Searching for {}", name);
-
-
     println!("Tentative de connexion au serveur...");
     match TcpStream::connect("127.0.0.1:1234") {
-        Ok(_) => {
+        Ok(stream) => {
             println!("Connexion au serveur réussie !");
+            exchange_with_server(stream);
         }
         Err(e) => {
             println!("La connexion au serveur a échoué : {}", e);
